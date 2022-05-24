751d32c1f0af072b21b5c79da96264f987163568
Vika Titaeva <vika.titaeva.2004@mail.ru>
2022-05-24 20:54:49 +0700

    все до 18 стр. 1 часть

--- app/src/main/java/com/example/practice/cinema.java
+++ app/src/main/java/com/example/practice/cinema.java
@@ -3,12 +3,17 @@ package com.example.practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class cinema extends AppCompatActivity {
-
+TextView tvInfo;
+EditText tvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_cinema );
+        tvInfo=(TextView) findViewById( R.id.tvInfo );
+        tvName=(EditText) findViewById( R.id.editTextTextPersonName );
     }
 }
